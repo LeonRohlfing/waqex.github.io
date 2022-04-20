86ed94569a73d970bbfacbdb747d37a382fb8079
Freddy880 <marks.florian123@gmail.com>
2022-04-20 13:14:50 +0200

    Ertellen eines auklapparen Menues

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function OpenMenue() {
-    
+    /*Display of the nav elements */
+    var navHeader = document.getElementById("navigation");
+    navHeader.style.display = "flex";
+    /*Display of teh button*/
+    var buttonToggle = document.getElementById("toggleMenue");
+    buttonToggle.style.display = "none";
+
+    /*Display of the close Button*/
 }
