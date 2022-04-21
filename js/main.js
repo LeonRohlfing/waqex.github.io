f302d8836650aa8dc4b9124ebe7258bcbd0ca4e8
Freddy880 <81533091+Freddy880@users.noreply.github.com>
2022-04-21 18:02:58 +0200

    Index.html Mobilunterstützung erfolgreich

--- js/main.js
+++ js/main.js
@@ -7,4 +7,29 @@ function OpenMenue() {
     buttonToggle.style.display = "none";
 
     /*Display of the close Button*/
+    var closeButton = document.getElementById("menueSchließen");
+    closeButton.style.display = "block";
+}
+function closeMenue() {
+    if (window.innerWidth < 969) {
+        /*Display of the nav elements */
+        var navHeader = document.getElementById("navigation");
+        navHeader.style.display = "none";
+        /*Display of teh button*/
+        var buttonToggle = document.getElementById("toggleMenue");
+        buttonToggle.style.display = "flex";        
+    }else{
+        /**
+         * RESET IF WINDOW SIZE HAS CHANGED WHILE USING WEBSITE
+         */
+        /*Display of the nav elements */
+        var navHeader = document.getElementById("navigation");
+        navHeader.style.display = "";
+        /*Display of teh button*/
+        var buttonToggle = document.getElementById("toggleMenue");
+        buttonToggle.style.display = "";        
+    }
+    /*Display of the close Button*/
+    var closeButton = document.getElementById("menueSchließen");
+    closeButton.style.display = "none";
 }
